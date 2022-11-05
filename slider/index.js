186da0e1ae9b92e3e7941b489180e7efa25643a1
Nana Pisanko <bonjozzy@gmail.com>
2022-11-05 18:08:43 +0300

    feat: add slider

---
+++ slider/index.js
@@ -0,0 +1,10 @@
+const slides = document.querySelectorAll('.slide');
+
+for (const slide of slides) {
+	slide.addEventListener('click', function(){
+		slides.forEach(slide => {
+			slide.classList.remove('active');
+		})
+		slide.classList.add('active');
+	})
+}
